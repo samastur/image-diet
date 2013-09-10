28b16a355177ad0f52860d9598cca5198635224f
Marko Samastur <markos@gaivo.net>
2013-09-10 10:06:11 +0200

    Update version
    Updated minor version for release of gifsicle fix.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author="Marko Samastur",
     author_email="markos@gaivo.net",
     name='image-diet',
-    version='0.7',
+    version='0.7.1',
     description='Remove unnecessary bytes from images',
     long_description=long_description,
     url='https://github.com/samastur/image-diet/',
